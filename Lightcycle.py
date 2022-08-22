cc1a730fe71379d3609246eb778704ec72a774c5
Taran Nulu <taran.nulu@gmail.com>
2022-08-22 13:04:51 -0700

    Implement first cut of Lightcycle game

---
+++ Lightcycle.py
@@ -0,0 +1,32 @@
+import pygame
+
+from Direction import *
+
+class Lightcycle:
+    SIZE = 10
+    VELOCITY = 10
+
+    def __init__(self, window, color, x, y, direction, up_key, down_key, left_key, right_key) -> None:
+        self.window = window
+        self.window_width, self.window_height = 700, 700 # window.get_size()
+        self.color = color
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.up_key = up_key
+        self.down_key = down_key
+        self.left_key = left_key
+        self.right_key = right_key
+    
+    def draw(self):
+        pygame.draw.rect(self.window, self.color, pygame.Rect(self.x, self.y, self.SIZE, self.SIZE))
+
+    def handle_key_press(self, keys_pressed):
+        if keys_pressed[self.up_key] and self.y - self.VELOCITY > 0:
+            self.y -= self.VELOCITY
+        if keys_pressed[self.down_key] and self.y + self.VELOCITY + self.SIZE < self.window_height:
+            self.y += self.VELOCITY
+        if keys_pressed[self.left_key] and self.x - self.VELOCITY > 0:
+            self.x -= self.VELOCITY
+        if keys_pressed[self.right_key] and self.x + self.VELOCITY + self.SIZE < self.window_width:
+            self.x += self.VELOCITY
---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame
+
+from Direction import *
+from Lightcycle import *
+
+width, height = 700, 700
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Tron Lightcycles")
+
+PADDING = 5
+
+FPS = 20
+vel = 10
+
+white = (255, 255, 255)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+
+def main():
+    player1 = Lightcycle(win, red, PADDING, PADDING, Direction.RIGHT, pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d)
+    player2 = Lightcycle(win, blue, width - Lightcycle.SIZE - PADDING, height - Lightcycle.SIZE - PADDING, Direction.LEFT, pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT)    
+
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+        
+        keys_pressed = pygame.key.get_pressed()
+        player1.handle_key_press(keys_pressed)
+        player2.handle_key_press(keys_pressed)
+
+        player1.draw()
+        player2.draw()
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
