42efb6c30124fd616a28537734afa6c693343a7e
Taran Nulu <taran.nulu@gmail.com>
2022-08-25 15:51:52 -0700

    Detect out of bounds and collisions

--- Lightcycle.py
+++ Lightcycle.py
@@ -1,14 +1,22 @@
+from turtle import back
 import pygame
+import enum
 
 from Direction import *
+
+class LightcycleStatus(Enum):
+    OK = 1
+    OUT_OF_BOUNDS = 2
+    COLLISION = 3
 
 class Lightcycle:
     SIZE = 10
     VELOCITY = 10
 
-    def __init__(self, window, color, x, y, direction, up_key, down_key, left_key, right_key) -> None:
+    def __init__(self, window, backgroundColor, color, x, y, direction, up_key, down_key, left_key, right_key) -> None:
         self.window = window
         self.window_width, self.window_height = 700, 700 # window.get_size()
+        self.backgroundColor = backgroundColor
         self.color = color
         self.x = x
         self.y = y
@@ -18,17 +26,25 @@ class Lightcycle:
         self.left_key = left_key
         self.right_key = right_key
     
-    def draw(self):
-        pygame.draw.rect(self.window, self.color, pygame.Rect(self.x, self.y, self.SIZE, self.SIZE))
-        if self.direction == Direction.UP and self.y - self.VELOCITY > 0:
+    def draw(self) -> LightcycleStatus:
+        if self.direction == Direction.UP:
             self.y -= self.VELOCITY
-        if self.direction == Direction.DOWN and self.y + self.VELOCITY + self.SIZE < self.window_height:
+        if self.direction == Direction.DOWN:
             self.y += self.VELOCITY
-        if self.direction == Direction.LEFT and self.x - self.VELOCITY > 0:
+        if self.direction == Direction.LEFT:
             self.x -= self.VELOCITY
-        if self.direction == Direction.RIGHT and self.x + self.VELOCITY + self.SIZE < self.window_width:
+        if self.direction == Direction.RIGHT:
             self.x += self.VELOCITY
 
+        status = LightcycleStatus.OK
+        if self.window.get_at((self.x, self.y)) != self.backgroundColor:
+            status = LightcycleStatus.COLLISION
+        elif self.x <= 0 or self.x >= self.window_width or self.y <= 0 or self.y >= self.window_height:
+            status = LightcycleStatus.OUT_OF_BOUNDS
+
+        pygame.draw.rect(self.window, self.color, pygame.Rect(self.x, self.y, self.SIZE, self.SIZE))
+
+        return status
 
     def handle_key_press(self, keys_pressed):
         if keys_pressed[self.up_key] and self.direction != Direction.DOWN:
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 from Direction import *
 from Lightcycle import *
 
+pygame.font.init()
+
 width, height = 700, 700
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Tron Lightcycles")
@@ -12,13 +14,22 @@ PADDING = 10
 FPS = 20
 vel = 10
 
+black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 blue = (0, 0, 255)
 
+winner_font = pygame.font.SysFont('comicsans', 100)
+
+def draw_winner(text):
+    draw_text = winner_font.render(text, 1, white)
+    win.blit(draw_text, (width/2 - draw_text.get_width()/2, height/2 - draw_text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(3000)
+
 def main():
-    player1 = Lightcycle(win, red, PADDING, PADDING, Direction.RIGHT, pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d)
-    player2 = Lightcycle(win, blue, width - Lightcycle.SIZE - PADDING, height - Lightcycle.SIZE - PADDING, Direction.LEFT, pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT)    
+    player1 = Lightcycle(win, black, red, PADDING, PADDING, Direction.RIGHT, pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d)
+    player2 = Lightcycle(win, black, blue, width - Lightcycle.SIZE - PADDING, height - Lightcycle.SIZE - PADDING, Direction.LEFT, pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT)    
 
     clock = pygame.time.Clock()
     run = True
@@ -34,8 +45,19 @@ def main():
         player1.handle_key_press(keys_pressed)
         player2.handle_key_press(keys_pressed)
 
-        player1.draw()
-        player2.draw()
+        player1Status = player1.draw()
+        player2Status = player2.draw()
+
+        if player1Status != LightcycleStatus.OK and player2Status != LightcycleStatus.OK:
+            draw_winner("It's a draw!")
+            run = False
+        elif player1Status != LightcycleStatus.OK:
+            draw_winner("Blue player wins!")
+            run = False
+        elif player2Status != LightcycleStatus.OK:
+            draw_winner("Red player wins!")
+            run = False
+        
         pygame.display.update()
 
 if __name__ == "__main__":
